0ba2d96a2a1ace1ae856e40953718db90d1605c5
Pääkkönen Petteri N <pepa@da5-cs-b221-07.pc.helsinki.fi>
2017-03-16 15:16:21 +0200

    Sovellus alustettu ja versionhallinta otettu käyttöön

--- config/environment.sh
+++ config/environment.sh
@@ -3,5 +3,5 @@
 # korvaa tämä omalla käyttäjätunnuksellasi
 USERNAME="pepa"
 # korvaa tämä haluamallasi kansion nimellä
-PROJECT_FOLDER="tietokanta"
+PROJECT_FOLDER="vaalikoneisto"
 # sovelluksesi tulee sijaitsemaan osoitteessa USERNAME.users.cs.helsinki.fi/PROJECT_FOLDER
