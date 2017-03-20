700c0776f8ec814483eed656fe6586765f16585e
Pääkkönen Petteri N <pepa@da5-cs-bk107-08.helsinki.fi>
2017-03-20 17:46:34 +0200

    muutaman sivun lisays ja linkit niihin readmessa

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -4,7 +4,28 @@
 
     public static function index(){
       // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  echo 'Tämä on etusivu!';
+//   	  echo 'Tämä on etusivu!';
+        View::make('aloitus.html');
+    }
+    
+     public static function esittely(){
+      // Testaa koodiasi täällä
+      View::make('esittely.html');
+    }
+    
+    public static function hallinto(){
+      // Testaa koodiasi täällä
+      View::make('hallinto.html');
+    }
+    
+    public static function tulosPuolueittain(){
+      // Testaa koodiasi täällä
+      View::make('tulosPuolueittain.html');
+    }
+    
+    public static function lisaaKysymys(){
+      // Testaa koodiasi täällä
+      View::make('uusiKysymys.html');
     }
 
     public static function sandbox(){
--- config/routes.php
+++ config/routes.php
@@ -7,3 +7,19 @@
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
   });
+  
+   $routes->get('/esittely', function() {
+    HelloWorldController::esittely();
+  });
+  
+   $routes->get('/hallinto', function() {
+    HelloWorldController::hallinto();
+  });
+  
+  $routes->get('/tiettyVastaus', function() {
+    HelloWorldController::tulosPuolueittain();
+  });
+  
+  $routes->get('/lisaaKysymys', function() {
+    HelloWorldController::lisaaKysymys();
+  });
