71a12d35e4d1152d8a638a38b70b1fb3a72cef06
Pääkkönen Petteri N <pepa@da5-cs-bk107-08.helsinki.fi>
2017-03-22 17:11:23 +0200

    sql tiedostojen paivitys

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,11 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Puolue (nimi) VALUES ('Keskusta');
+INSERT INTO Puolue (nimi) VALUES ('SDP');
+
+INSERT INTO Kysymys (kysymys, istunto, paivamaara, linkki) VALUES ('Kansalaisaloite avioliiton säilyttämisestä aidosti tasa-arvoisena, miehen ja naisen välisenä liittona ja sukupuolineutraalin avioliittolain kumoamisesta','11','2017-2-22','https://www.eduskunta.fi/FI/Vaski/sivut/aanestys.aspx?aanestysnro=1&istuntonro=11&vuosi=2017'); 
+
+INSERT INTO Tulos (puolue_id, kysymys_id, tulos, jaa, ei ,tyhja, poissa) VALUES ((SELECT id FROM Puolue WHERE nimi='SDP'),(SELECT id FROM Kysymys WHERE istunto='11'),'ei','17','22','0','10');
+
+INSERT INTO Vastaus (kysymys_id, vastaus) VALUES ((SELECT id FROM Kysymys WHERE istunto='11'), 'ei');
+
+
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,32 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Puolue(
+    id SERIAL PRIMARY KEY,
+    nimi varchar(50) NOT NULL
+);
+
+CREATE TABLE Kysymys(
+    id SERIAL PRIMARY KEY,
+    kysymys varchar(200) NOT NULL,
+    istunto INTEGER NOT NULL,
+    paivamaara DATE,
+    linkki varchar(200) NOT NULL
+);
+
+CREATE TABLE Tulos(
+    id SERIAL PRIMARY KEY, 
+    puolue_id INTEGER REFERENCES Puolue(id),
+    kysymys_id INTEGER REFERENCES Kysymys(id),
+    tulos  varchar(10) NOT NULL,
+    jaa INTEGER,
+    ei INTEGER,
+    tyhja INTEGER,
+    poissa INTEGER
+);
+
+CREATE TABLE Vastaus(
+    id SERIAL PRIMARY KEY,
+    kysymys_id INTEGER REFERENCES Kysymys(id),
+    vastaus varchar(10) NOT NULL
+);
+
+
+    
